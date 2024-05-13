70d0be5b7679dbd9614282aa2f30212f9d52cb62
JoseAngel <Jangelgiron@gmail.com>
2024-05-13 10:00:00 +0200

    Commit de prueba, actividad6.3 - Jose Angel - Rama main modificada

--- Prueba/Prueba.java
+++ Prueba/Prueba.java
@@ -3,6 +3,9 @@ package Prueba;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Prueba para la actividad 6.3");
+        System.out.println("Este es un cambio distinto al anterior");
+        System.out.println("Lo cual dara error al fusionar las ramas");
+
 
     }
 }
