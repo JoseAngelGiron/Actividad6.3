65ea95d9225a13b6fb8eb37deefc89a4e13b6533
JoseAngel <Jangelgiron@gmail.com>
2024-05-13 09:56:52 +0200

    Commit de prueba, actividad6.3 - Jose Angel - nueva rama

--- Prueba/Prueba.java
+++ Prueba/Prueba.java
@@ -3,6 +3,7 @@ package Prueba;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Prueba para la actividad 6.3");
+        System.out.println("Archivo modificado, :)");
 
     }
 }
